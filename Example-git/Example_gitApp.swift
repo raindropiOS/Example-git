d1284514e19afbdc25d7e12bec16c71c37d4e76a
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-03 17:21:23 +0900

    example init

---
+++ Example-git/Example_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_gitApp.swift
+//  Example-git
+//
+//  Created by 박시현 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct Example_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
